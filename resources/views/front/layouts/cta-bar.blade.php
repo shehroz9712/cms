03a97396926126b96c8ab21feec3f34eefb8af86
shehroz9712 <shehrysoftologics@gmail.com>
2023-09-29 22:35:58 +0500

    add text

--- resources/views/front/layouts/cta-bar.blade.php
+++ resources/views/front/layouts/cta-bar.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="justify-content-center row">
             <div class="col-lg-12 text-center">
-                <h3 class="fs-1 bold-heading">Take the First Today to building your brand </h3>
+                <h3 class="fs-1 bold-heading">Take the First step Today to building your brand </h3>
                 <!-- <div class="button-container">
                             <a href="#" class='button'>WATCH VIDEO</a>
                         </div> -->
